fb3ef2e1108943f640e735f7fe47670deff55644
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-04-26 23:12:55 +0530

    Refactor:To Write test cases to match for no parameter method

--- MoodAnalyserExceptions.java
+++ MoodAnalyserExceptions.java
@@ -2,17 +2,25 @@ package exceptionTest;
 
 public  class MoodAnalyserException {
 	
-        static final String message=" ";
+	static final String message=" ";
 	
 	public static String MoodAnalyserException() {
 		
 		if(message.contains("Sad")) 
-		     return "SAD";
+             	    return "SAD";
 	        else    
-		     return "HAPPY";
+		    return "HAPPY";
 	}
-
+	
+    	public static String MoodAnalyserException(String message) {
+		
+		if(message.contains("Sad")) 
+		    return "SAD";
+	        else    
+		    return "HAPPY";
+	}
 }
+
 
 
 package exceptionTest;
@@ -25,7 +33,8 @@ public class MoodAnalyserExceptionTest {
 	@Test
 	public void givenMood_shouldReturnHappy() {
 		String message="I am in Happy mood";
-		String mood=MoodAnalyserException.MoodAnalyserException();
+		String mood=MoodAnalyserException.MoodAnalyserException(message);
+		System.out.println(mood);
 		mood=MoodAnalyserException.MoodAnalyserException();
 		System.out.println(mood);
 		assertEquals("HAPPY", mood);
@@ -34,7 +43,8 @@ public class MoodAnalyserExceptionTest {
 	@Test
 	public void givenMood_shouldReturnSad() {
 		String message="I am in Sad mood";
-		String mood=MoodAnalyserException.MoodAnalyserException();
+		String mood=MoodAnalyserException.MoodAnalyserException(message);
+		System.out.println(mood);
 		mood=MoodAnalyserException.MoodAnalyserException();
 		System.out.println(mood);      
 		assertEquals("HAPPY", mood);
