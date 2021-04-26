66f1706383bc2a1d00559a3d017aa235d8294ccf
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-04-26 22:35:01 +0530

    Added:To Write test case to match mood

--- MoodAnalyserExceptions.java
+++ MoodAnalyserExceptions.java
@@ -1,15 +1,14 @@
 package exceptionTest;
 
 public  class MoodAnalyserException {
-	
-	public static String MoodAnalyserException(String message) {
-		if(message.equals("SAD"))
-		   return "SAD";
-		else
-		   return "HAPPY";
-	}
-	
 
+	public static String MoodAnalyserException (String message) {
+		
+		if(message.contains("Sad")) 
+		       return "SAD";
+	        else    
+		       return "HAPPY";
+	}
 }
 
 package exceptionTest;
@@ -21,14 +20,14 @@ public class MoodAnalyserExceptionTest {
 	
 	@Test
 	public void givenMood_shouldReturnHappy() {
-		String message="HAPPY";
+		String message="I am in Happy mood";
 		String mood=MoodAnalyserException.MoodAnalyserException(message);
 		assertEquals("HAPPY", mood);
 	}
 	
 	@Test
 	public void givenMood_shouldReturnSad() {
-		String message="SAD";
+		String message="I am in Sad mood";
 		String mood=MoodAnalyserException.MoodAnalyserException(message);
 		assertEquals("SAD", mood);
 	}
