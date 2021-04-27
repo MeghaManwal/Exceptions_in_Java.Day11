0011b828c0f89d0ec04d22fe93f84fd684c1ad2a
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-04-27 06:39:27 +0530

    Refactor:To handle exception when user provides Invalid mood and return CustomException

--- MoodAnalyserExceptions.java
+++ MoodAnalyserExceptions.java
@@ -4,29 +4,34 @@ public  class MoodAnalyserException {
 	
 	static final String message=" ";
 	
-	public static String MoodAnalyserException() {
-	    try {
+	public static String MoodAnalyserException() throws CustomException {
+	 try {
 		  if(message.contains("Sad")) 
 		       return "SAD";
 	          else    
 		       return "HAPPY";
-	         } catch (NullPointerException ae) {
-	              return "HAPPY";
-	         }
+	     } catch (NullPointerException ae) {
+	              throw new CustomException("Invalid Mood! Please provide the valid mood ");
+	     }
 	}
 	
-        public static String MoodAnalyserException(String message) {
-            try {
-		  if(message.contains("Sad")) 
-		       return "SAD";
-	          else    
-		       return "HAPPY";
-    	        } catch (NullPointerException ae) {
-	           return "HAPPY";
-	        }
-       }
+        public static String MoodAnalyserException(String message) throws CustomException {
+          try {
+		   if(message.contains("Sad")) 
+			return "SAD";
+	           else    
+		        return "HAPPY";
+    	       } catch (NullPointerException ae) {
+    		        throw new CustomException("Invalid Mood! Please provide valid mood ");
+	       }
+	}
 }
 
+class CustomException extends Exception{
+	public CustomException(String msg) {
+	   super(msg);
+	}
+}
 
 
 package exceptionTest;
@@ -36,29 +41,37 @@ import org.junit.Test;
 
 public class MoodAnalyserExceptionTest {
 
-	@Test
+        @Test
 	public void givenMood_shouldReturnHappy() {
 		String message = null;
-		
+	  try {
 		String mood=MoodAnalyserException.MoodAnalyserException(message);
 		System.out.println(mood);
 		
+		
 		mood=MoodAnalyserException.MoodAnalyserException();
 		System.out.println(mood);
-		
+		 
 		assertEquals("HAPPY", mood);
+	      } catch (CustomException e) {
+		   System.out.println(e);
+	      }
 	}
 	
 	@Test
-	public void givenMood_shouldReturnSad() {
+	public void givenMood_shouldReturnSad() throws CustomException {
 		String message =null;
-		
+	  try {
 		String mood=MoodAnalyserException.MoodAnalyserException(message);
 		System.out.println(mood);
 		
 		mood=MoodAnalyserException.MoodAnalyserException();
 		System.out.println(mood);  
 		
-		assertEquals("HAPPY", mood);
+	 	assertEquals("HAPPY", mood);
+	       }catch (CustomException e) {
+			System.out.println(e);
+	       } 
 	}
+
 }
